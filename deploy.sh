f9dac1cbc9e9b12ea267e1319954b1fc89ff556e
Ilan JOURNO <ilan.journo@nolina.io>
2024-08-12 03:25:43 +0300

    :sparkles: Add some features

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 set +e
 
-docker build -f Dockerfile.production -t europe-west9-docker.pkg.dev/claudeallouche/api/api:lasted . || true &&
-docker push europe-west9-docker.pkg.dev/claudeallouche/api/api:lasted || true &&
-gcloud run deploy api --image europe-west9-docker.pkg.dev/claudeallouche/api/api:lasted --platform managed --region us-central1 --allow-unauthenticated || true &&
+docker build -f Dockerfile.production -t europe-west9-docker.pkg.dev/claudeallouche/ahuza/ahuza:lasted . || true &&
+docker push europe-west9-docker.pkg.dev/claudeallouche/ahuza/ahuza:lasted || true &&
+gcloud run deploy ahuza --image europe-west9-docker.pkg.dev/claudeallouche/ahuza/ahuza:lasted --platform managed --region europe-west1 --allow-unauthenticated || true &&
 
 set -e
