6abf614759d890839b05911577f037937119a3e9
Ilan JOURNO <ilan.journo@nolina.io>
2024-10-16 17:36:46 +0300

    :bug: Fix hebrew text

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,16 @@ use App\Http\Controllers\Api\KeywordController;
 use App\Http\Controllers\Api\KeywordGroupController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->prefix('/api')->group(function () {
-    Route::get('/categories', [CategoriesController::class, 'index']);
-    Route::get('/internal-categories', [InternalCategoryController::class, 'index']);
-    Route::get('/keywords', [KeywordController::class, 'index']);
-    Route::get('/keyword-groups', [KeywordGroupController::class, 'index']);
-    Route::post('/files', [FileStorageController::class, 'store']);
-    Route::get('/files/list', [FileStorageController::class, 'list'])->name('files.list');
-    Route::get('/files', [FileStorageController::class, 'index']);
-});
+Route::prefix('/api')->group(function () {
+    Route::middleware('auth')->group(function () {
+        Route::get('/categories', [CategoriesController::class, 'index']);
+        Route::get('/internal-categories', [InternalCategoryController::class, 'index']);
+        Route::get('/keywords', [KeywordController::class, 'index']);
+        Route::get('/keyword-groups', [KeywordGroupController::class, 'index']);
+        Route::post('/files', [FileStorageController::class, 'store']);
+        Route::get('/files/list', [FileStorageController::class, 'list'])->name('files.list');
+        Route::get('/files', [FileStorageController::class, 'index']);    
+    });
 
-Route::get('/articles', [ArticlesController::class, 'index']);
+    Route::get('/articles', [ArticlesController::class, 'index']);
+});
